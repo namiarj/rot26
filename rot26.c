c6545e60290fc1ea5e1eda8af1cca1ae9f401d8c
Nami Arjmandi <namiarjmandi@gmail.com>
2019-11-29 00:54:08 +0330

    hardening against buffer overflow attacks

--- rot26.c
+++ rot26.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
+int main() {
 	char buffer[512];
 	memset(&buffer, 0, sizeof buffer);
 	
-	printf("Enter the cipher text:\n");
+	printf("Enter the cipher text: (max 512 chars)\n");
 	scanf("%s", &buffer);
 
 	printf("\nEnciphered text: \n%s\n", buffer);
